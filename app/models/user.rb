24f03d88646d6ce1819ba92642d84d19468d6751
takeru.hashimoto <hstake25@gmail.com>
2022-03-15 20:38:19 +0900

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :books
-  has_one_attached :profile_image
+          :recoverable, :rememberable, :validatable
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, presence: true
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  has_one_attached :profile_image
+
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
