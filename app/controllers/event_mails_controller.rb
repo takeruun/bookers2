65fc60c005567185c226d808ca714778b13a8cd5
takeru.hashimoto <hstake25@gmail.com>
2022-03-18 08:00:00 +0900

    メール送信

--- app/controllers/event_mails_controller.rb
+++ app/controllers/event_mails_controller.rb
@@ -5,9 +5,15 @@ class EventMailsController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
     @event_mail = EventMail.new(event_mail_params)
-    @event_mail.group_id = params[:group_id]
-    @event_mail.save
+    @event_mail.group_id = @group.id
+    if @event_mail.save
+      to_mails = @group.members.pluck(:email).join(',')
+      EventMailMailer.send_mail(to_mails, @event_mail).deliver_now
+    else
+      render :new
+    end
   end
 
   private
